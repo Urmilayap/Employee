c28c51165e01f81fc01a20e3f98d3f31b3c22fa7
Urmila Sheral <118338442+Urmilayap@users.noreply.github.com>
2023-02-02 17:05:30 +0530

    Revert "added post req. api"

--- src/controllers/departmentDetails.controller.js
+++ src/controllers/departmentDetails.controller.js
@@ -1,48 +1,29 @@
-/* eslint-disable camelcase */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-const { error, success } = require('@yapsody/lib-handlers')
-const { departmentDetailsService } = require('../services')
-const { departmentDetailsValidation } = require('../validations')
-const {  departmentDetailsValidation,getId, getListValidation } = require('../validations')
+const { departmentDetailsService } = require('../services');
+const { error, success } = require('@yapsody/lib-handlers');
+const {  departmentDetailsValidation,getId, getListValidation } = require('../validations');
 
-// create Department
-const addDepartmentdetails = async (req, res, next) => {
-  try {
-    const {
-      min_income, max_income, description, introduced_date,
-    } = await departmentDetailsValidation.validateAsync(req.body);
-    const departmentdetails = await departmentDetailsService.addDepartmentdetails({
-      min_income, max_income, description, introduced_date,
-    });
-    console.log('------------>', departmentdetails);
+
+
+//create Department
+  const addDepartmentdetails = async (req ,res ,next) => {
+    try {
+    const {  min_income, max_income, description, introduced_date }= await departmentDetailsValidation.validateAsync(req.body);
+    const departmentdetails = await departmentDetailsService.addDepartmentdetails({ 
+      min_income, max_income, description, introduced_date });
+      console.log('------------>',departmentdetails);
     return success.handler({ departmentdetails }, req, res, next);
-  } catch (err) {
-    switch (err.department_id) {
-      case 'SequelizeUniqueConstraintError':
-        err.custom_key = 'departmentConflict';
-        err.message = `department with id ${req.body.department_id} already exists`;
-        break;
-      default:
-        break;
+    } catch (err) {
+      switch (err.department_id) {
+        case 'SequelizeUniqueConstraintError':
+          err.custom_key = 'departmentConflict';
+          err.message = `department with id ${req.body.department_id} already exists`;
+          break;
+        default:
+          break;
+      }
+      return error.handler(err, req, res, next);
     }
-    return error.handler(err, req, res, next);
-  }
-};
-
-const getAllDepartmentdetails = async (req, res, next) => {
-  const reqData = { ...req.query };
-  if (reqData.ids) {
-    reqData.ids = reqData.ids.split(';');
-  }
-  try {
-    const validate = await getListValidation.validateAsync(reqData);
-    const departments = await departmentDetailsService.getAllDepartmentdetails();
-    return success.handler({ departments }, req, res, next);
-  } catch (err) {
-    return error.handler(err, req, res, next);
-  }
-};
+  };
   //Get Department by ID
  const getById = async (req, res, next) => {
   const { departmentdetailsId } = req.params;
@@ -67,5 +48,4 @@ const getAllDepartmentdetails = async (req, res, next) => {
   }
 };
 
-  module.exports = { addDepartmentdetails, getAllDepartmentdetails, getById, getAll };
-  
+  module.exports = { addDepartmentdetails, getById, getAll };
--- src/validations/add-multiple.validation.js
+++ src/validations/add-multiple.validation.js
@@ -2,30 +2,31 @@ const Joi = require('joi');
 const yapValidations = require('@yapsody/lib-validations');
 
 module.exports = Joi.array().items(
-  Joi.object().keys({
+        Joi.object().keys({
 
-    employee_id: yapValidations
-      .id
-      .required()
-      .label('Employee ID'),
-    first_name: yapValidations
-      .name
-      .required()
-      .label('First Name'),
-    last_name: yapValidations
-      .name
-      .required()
-      .label('Last Name'),
-    email_id: yapValidations
-      .email
-      .required()
-      .label('Email Id'),
-    phone_no: yapValidations
-      .phoneNo
-      .required()
-      .label('Phone No'),
-    address: yapValidations
-      .description
-      .label('Address'),
-  }),
+            employee_id: yapValidations
+            .id
+            .required()
+            .label('Employee ID'),
+            first_name: yapValidations
+            .name
+            .required()
+            .label('First Name'),
+            last_name: yapValidations
+            .name
+            .required()
+            .label('Last Name'),
+            email_id: yapValidations
+            .email
+            .required()
+            .label('Email Id'),
+            phone_no: yapValidations
+            .phoneNo
+            .required()
+            .label('Phone No'),
+            address: yapValidations
+            .description
+            .label('Address'),
+})
 );
+
